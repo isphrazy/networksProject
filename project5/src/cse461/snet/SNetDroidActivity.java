ac401328e964d3989f067ac93594d7dd2f242a4d
Pingyang He <pingyh@cs.washington.edu>
2012-05-27 14:00:33 -0700

    check in

---
+++ project5/src/cse461/snet/SNetDroidActivity.java
@@ -0,0 +1,13 @@
+package cse461.snet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SNetDroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
