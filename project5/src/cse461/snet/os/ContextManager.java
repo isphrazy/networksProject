224d20fcb4e274347dd43f906b64488e048bcaca
Pingyang He <pingyh@cs.washington.edu>
2012-05-27 14:22:21 -0700

    check in

---
+++ project5/src/cse461/snet/os/ContextManager.java
@@ -0,0 +1,12 @@
+package cse461.snet.os;
+
+import android.content.Context;
+
+public class ContextManager {
+
+    public static Context getContext() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
