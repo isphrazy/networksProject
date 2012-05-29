d4e9cc3dab4cc05e90dd62d5e4a0c2fd3d49e00c
Pingyang He <pingyh@cs.washington.edu>
2012-05-29 13:37:36 -0700

    check in

--- project5/gen/cse461/snet/R.java
+++ project5/gen/cse461/snet/R.java
@@ -26,7 +26,7 @@ public final class R {
         public static final int linearLayout3=0x7f050001;
         public static final int my_pic_iv=0x7f050006;
         public static final int scrollView1=0x7f050000;
-        public static final int spinner1=0x7f05000d;
+        public static final int spinner=0x7f05000d;
         public static final int take_pic_bt=0x7f05000a;
         public static final int textView1=0x7f050003;
         public static final int textView2=0x7f050007;
--- project5/src/cse461/snet/UpdatePicPage.java
+++ project5/src/cse461/snet/UpdatePicPage.java
@@ -7,18 +7,29 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Spinner;
 
 public class UpdatePicPage extends Activity {
+    
+    Spinner spinner;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.update_layout);
+        
+        initVars();
     }
     
     
+    private void initVars() {
+        spinner = (Spinner) findViewById(R.id.spinner);
+    }
+
+
     public void beFriend(View view){
-        Log.e("befiend", "befriend");
+        
     }
     
     
