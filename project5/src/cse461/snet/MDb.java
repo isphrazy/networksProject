e4b5cbe80afebb4429689ec6bfc74ff554b5b5c3
Pingyang He <pingyh@cs.washington.edu>
2012-05-30 11:32:25 -0700

    check in

--- project5/src/cse461/snet/MDb.java
+++ project5/src/cse461/snet/MDb.java
@@ -1,14 +1,22 @@
 package cse461.snet;
 
+import edu.uw.cs.cse461.sp12.util.DB461.DB461Exception;
+import edu.uw.cs.cse461.sp12.util.DB461SQLite;
+
+/**
+ * wrapper class over DB461SQLite
+ * @author Pingyang He
+ *
+ */
 public class MDb {
 
-    private static MDb instance;
+    private static DB461SQLite instance;
     
     private MDb(){}
     
-    public static MDb getInstance(){
+    public static DB461SQLite getInstance(String dbName) throws DB461Exception{
         if(instance == null){
-            instance = new MDb();
+            instance = new DB461SQLite(dbName);
         }
         
         return instance;
